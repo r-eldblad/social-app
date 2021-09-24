c05f6e46bc5793744d93f55a83907f5db3eb6929
Rasmus Eldblad <r.eldblad@protonmail.com>
2021-09-24 16:25:43 +0200

    This commit will make the user able to register new accounts

--- src/pages/RegisterPage/RegisterPage.js
+++ src/pages/RegisterPage/RegisterPage.js
@@ -1,9 +1,30 @@
+import { useState } from "react";
 const RegisterPage = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const user = { name, email, password };
+    console.log(user);
+    fetch("http://localhost:8000/api/user/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(user),
+    }).then((response) => {
+      console.log(response);
+      console.log("Användare skapad!");
+    });
+  };
   return (
     <div>
       <div>
         <h1 className="text-4xl my-7 text-center">Register</h1>
-        <form className="container mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <form
+          className="container mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+          onSubmit={handleSubmit}
+        >
           <div className="mb-4">
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
@@ -16,6 +37,7 @@ const RegisterPage = () => {
               id="name"
               type="text"
               placeholder="Name"
+              onChange={(e) => setName(e.target.value)}
             ></input>
           </div>
           <div className="mb-4">
@@ -30,6 +52,7 @@ const RegisterPage = () => {
               id="email"
               type="text"
               placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
             ></input>
           </div>
           <div className="mb-6">
@@ -44,12 +67,13 @@ const RegisterPage = () => {
               id="password"
               type="password"
               placeholder="******************"
+              onChange={(e) => setPassword(e.target.value)}
             ></input>
           </div>
           <div className="flex items-center justify-between">
             <button
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline uppercase"
-              type="button"
+              type="submit"
             >
               Register
             </button>
